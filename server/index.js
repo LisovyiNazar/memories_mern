ab2e0d53c40e8d0274cf412b9afabea3888b1319
Назар Лісовий <nazarlisovij@forstnazar.local>
2023-02-28 17:23:46 +0200

    create server base structure

---
+++ server/index.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import * as dotenv from 'dotenv'
+
+const app = express()
+
+app.use(bodyParser.json({ limit: '30mb', extended : true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended : true }))
+app.use(cors())
+
+dotenv.config({
+    path : './config/.env.local'
+})
+
+const PORT = process.env.PORT | 4000
+
+mongoose.set('strictQuery', false)
+mongoose.connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+}).catch((error) => {
+    console.log(error.message)
+})
