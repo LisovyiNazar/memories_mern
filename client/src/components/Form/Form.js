a0871288e450e26890924bb9deca8f44cc767c9d
Назар Лісовий <nazarlisovij@forstnazar.local>
2023-05-17 16:59:59 +0300

    Form redesign and fix bugs

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,14 +1,11 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { PostForEditContext } from '../../store/context/postForEdit.context'
-import { TextField, Button, Typography, Paper } from '@material-ui/core'
 import FileBase from 'react-file-base64'
 import { useDispatch } from 'react-redux'
 import { createPost, updatePost } from '../../store/actions/post.actions'
-import useStyles from './styles'
+import './index.scss'
 
 const Form = () => {
-    const classes = useStyles()
-
     const initInputValue = {
         creator: '',
         title: '',
@@ -45,69 +42,63 @@ const Form = () => {
     }
 
     return (
-        <Paper className={classes.paper}>
-            <form autoComplete='off' noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
-                <Typography variant='h6'>{ postForEdit ? 'Update' : 'Create' } a Memory</Typography>
-                <TextField 
-                    name='creator' 
-                    variant='outlined' 
-                    label='Creator' 
-                    fullWidth
+        <form
+            className='memories-form'
+            onSubmit={handleSubmit}
+        >
+            <h2>{ postForEdit ? 'Update' : 'Create' } a Memory</h2>
+            <div className="group">
+                <input 
+                    type="text"
+                    required
                     value={postData.creator}
                     onChange={(e) => setPostData({ ...postData, creator: e.target.value })}
                 />
-                <TextField 
-                    name='title' 
-                    variant='outlined' 
-                    label='Title' 
-                    fullWidth
+                <span className="bar"></span>
+                <label>Creator</label>
+            </div>
+            <div className="group">
+                <input 
+                    type="text" 
+                    required
                     value={postData.title}
                     onChange={(e) => setPostData({ ...postData, title: e.target.value })}
-                />
-                <TextField 
-                    name='message' 
-                    variant='outlined' 
-                    label='Message' 
-                    fullWidth
+                ></input>
+                <span className="bar"></span>
+                <label>Title</label>
+            </div>
+            <div className="group"> 
+                <input 
+                    type="text" 
+                    required
                     value={postData.message}
                     onChange={(e) => setPostData({ ...postData, message: e.target.value })}
-                />
-                <TextField 
-                    name='tags' 
-                    variant='outlined' 
-                    label='Tags' 
-                    fullWidth
+                ></input>
+                <span className="bar"></span>
+                <label>Message</label>
+            </div>
+            <div className="group">
+                <input 
+                    type="text" 
+                    required
                     value={postData.tags}
                     onChange={(e) => setPostData({ ...postData, tags: e.target.value.split(',') })}
-                />
-                <div className={classes.fileInput}>
-                    <FileBase 
-                        type='file'
-                        multiple={false}
-                        onDone={({base64}) => setPostData({ ...postData, selectedFile: base64 })}
-                    />
-                </div>
-                <Button 
-                    className={classes.buttonSubmit} 
-                    variant='contained' 
-                    color='primary' 
-                    size='large' 
-                    type='submit' 
-                    fullWidth 
-                >
-                    Submit
-                </Button>
-                <Button 
-                    variant='contained' 
-                    color='secondary' 
-                    size='small' 
-                    onClick={() => {clearForm()}} 
-                    fullWidth 
-                >
-                    Clear
-                </Button>
-            </form>
-        </Paper>
+                ></input>
+                <span className="bar"></span>
+                <label>Tags</label>
+            </div>
+            <div className="image">
+                <FileBase 
+                    type='file'
+                    multiple={false}
+                    onDone={({ base64 }) => setPostData({ ...postData, selectedFile: base64 })}
+                />   
+            </div>
+            <div className='button-group'>
+                <button type='submit' className='submit'>{ postForEdit ? 'Update' : 'Submit' }</button>
+                <button type='button' className='clear' onClick={() => clearForm()} >Clear</button>
+            </div>
+        </form>
     )
 }
 
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
     return (
         <div className='home-wrapper'>
             <Posts />
-            <div className='form-wrapper'>
+            <div className='sidebar'>
                 <Form />
             </div>
         </div>
